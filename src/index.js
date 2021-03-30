29f2efae8a174198e04c61448bd24ddf59bf21b4
Daria Viter <viter.daria@gmail.com>
2021-03-30 19:31:13 +0100

    index my changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 
@@ -8,6 +9,23 @@ ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+=======
+import App from './components/App.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+>>>>>>> Stashed changes
   </React.StrictMode>,
   document.getElementById('root'),
 );
