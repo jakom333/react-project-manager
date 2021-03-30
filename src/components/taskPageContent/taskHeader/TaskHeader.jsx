7b2c18111e162925bea25eceda9a797a6ec7782b
Dmytro Orgish <isnl.dd@gmail.com>
2021-03-30 21:50:36 +0300

    create task -page

---
+++ src/components/taskPageContent/taskHeader/TaskHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './TaskHeader.module.css';
+
+const SprintHeader = () => {
+  return (
+    <div className={styles.sprintHeaderContainer}>
+      <h2>sprints head</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro neque
+        cumque sapiente recusandae. Asperiores labore odio corrupti ipsa fugit
+        fugiat, esse blanditiis id sapiente omnis dolorum consequuntur vero
+        praesentium iusto. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Porro neque cumque sapiente recusandae. Asperiores labore odio
+        corrupti ipsa fugit fugiat, esse blanditiis id sapiente omnis dolorum
+        consequuntur vero praesentium iusto.
+      </p>
+    </div>
+  );
+};
+
+export default SprintHeader;
