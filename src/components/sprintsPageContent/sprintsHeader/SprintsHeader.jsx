73a26ca910ddb2ea16e3e1735fbc4f9a8a8b427a
Ivan Khailo <jakom333@gmail.com>
2021-03-31 20:27:08 +0300

    styles fix

--- src/components/sprintsPageContent/sprintsHeader/SprintsHeader.jsx
+++ src/components/sprintsPageContent/sprintsHeader/SprintsHeader.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SprintsHeader.module.css';
 import sprite from '../../../icons/symbol-defs.svg';
 import RoundButton from '../../../shared/roundButton/RoundButton';
 import ButtonShow from '../../../shared/buttonShow/ButtonShow';
+import MainModal from '../../../shared/mainModal/MainModal';
+import СreateSprintForm from '../../createSprintForm/CreateSprintForm';
 
 const SprintsHeader = () => {
-
-    return (<div  className={styles.boxHead}>
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div className={styles.boxHead}>
       <div className={styles.boxBack}>
-        <ButtonShow/>
+        <ButtonShow />
         <p className={styles.showProject}>Show projects</p>
       </div>
       <div className={styles.head}>
         <div className={styles.titleBox}>
           <div className={styles.title}>
             <h2 className={styles.pageTitle}>PROJECT NAME</h2>
-            <button type='button' className={styles.buttonFix}>
+            <button type="button" className={styles.buttonFix}>
               <svg className={styles.iconPencil}>
                 <use href={sprite + '#icon-pencil'}></use>
               </svg>
             </button>
           </div>
           <div className={styles.buttonBox}>
-            <RoundButton />
+            <RoundButton onClick={() => setShowModal(true)} />
             <p className={styles.pageText}>Create sprint</p>
           </div>
         </div>
       </div>
       <p className={styles.description}>Короткий опис проекту</p>
       <div className={styles.addPeopleBox}>
-        <button type='button' className={styles.buttonAddPeople}>
+        <button type="button" className={styles.buttonAddPeople}>
           <svg className={styles.iconAddPeople}>
             <use href={sprite + '#icon-add-group'}></use>
           </svg>
           <p className={styles.addPeopleText}>Add people</p>
         </button>
+        <MainModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          onClose={() => setShowModal(false)}
+        >
+          <СreateSprintForm />
+        </MainModal>
       </div>
-   </div>);
-
-}
+    </div>
+  );
+};
 
 export default SprintsHeader;
