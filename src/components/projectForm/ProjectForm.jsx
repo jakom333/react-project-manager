02b1ea55a4d740401d1ef213dcd334ce8cbaf566
Bogdan Lytvyn <72852244+BogdanLytvyn@users.noreply.github.com>
2021-04-01 10:29:09 +0300

    add temolate form

--- src/components/projectForm/ProjectForm.jsx
+++ src/components/projectForm/ProjectForm.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import styles from './ProjectForm.module.css'
 
+
 const ProjectForm = () => {
 
-    return (
-      <div>
-
-         <h3 className={styles.formTitle}>Create project</h3>
-
+  return (
+      <div className={styles.formBox}>
+        <form
+          className={styles.form}
+          onSubmit=""
+        >
+          <h2 className={styles.formTitle}>Create project</h2>
+          <input
+              autoComplete="off"
+              className={styles.input}
+              placeholder="name Project"
+              type="text"
+              value=''
+              name="name"
+              onChange=""
+              required
+            />
+          <input
+              autoComplete="off"
+              className={styles.input}
+              placeholder="description"
+              type="text"
+              value=''
+              name="number"
+              onChange=""
+              required
+            />
+          </form>
       </div>
     );
 
