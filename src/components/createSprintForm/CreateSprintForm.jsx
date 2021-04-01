d552935982b5452d2ddf51d8ff18d2ad16b81c12
Dmytro Orgish <isnl.dd@gmail.com>
2021-04-01 20:20:20 +0300

    addSprint create

--- src/components/createSprintForm/CreateSprintForm.jsx
+++ src/components/createSprintForm/CreateSprintForm.jsx
@@ -4,17 +4,27 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-
+import Button from '../../shared/button/Button';
+import { addSprint } from '../../redux/sprints/sprints-operations';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import styles from './CreateSprintForm.module.css';
 
 const formSchema = Yup.object().shape({
+<<<<<<< Updated upstream
   name: Yup.string().required('* Task name is a required field'),
   time: Yup.number('* Scheduled time has to be an appropriate number')
     .required('* Description is a required field')
+=======
+  title: Yup.string().required('* Sprint name is a required field'),
+  duration: Yup.number('* Duration has to be an appropriate number')
+    .required('* Duration is a required field')
+>>>>>>> Stashed changes
     .positive()
     .integer(),
 });
 
+<<<<<<< Updated upstream
 const СreateSprintForm = () => {
   const [startDate, setStartDate] = useState(new Date());
 
@@ -45,6 +55,90 @@ const СreateSprintForm = () => {
           onChange={date => setStartDate(date)}
         />
       </form>
+=======
+const CreateSprintForm = ({ onClose }) => {
+  const DatePickerField = () => {
+    return (
+      <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+    );
+  };
+
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div className={styles.formContainer}>
+      <h2 className={styles.titleForm}>Create a sprint</h2>
+      <Formik
+        initialValues={{
+          title: '',
+          endDate: '',
+          duration: '',
+        }}
+        validationSchema={formSchema}
+        onSubmit={async (values, { resetForm }) => {
+          addSprint(values);
+          dispatch(addSprint(values, params.projectId));
+          onClose();
+
+          resetForm({});
+          // написать здесь свою операцию  createSprint//
+        }}
+      >
+        <Form className={styles.form}>
+          <Field
+            className={styles.inputName}
+            name='title'
+            type='text'
+            placeholder='The name of the sprint'
+          />
+          <ErrorMessage
+            className={styles.errorName}
+            component='span'
+            name='title'
+          />
+          <div className={styles.form}>
+            <Field
+              className={styles.checkbox}
+              type='checkbox'
+              name='pastDays'
+              placeholder='Includes past days'
+            />
+            <label className={styles.checkboxLabel}>
+              <span>Includes previous days</span>
+              <Field
+                className={styles.inputTime}
+                name='endDate'
+                type='text'
+                placeholder='yyyy-mm-dd'
+              />
+            </label>
+
+            <ErrorMessage
+              className={styles.errorTime}
+              component='span'
+              name='endDate'
+            />
+            <label className={styles.checkboxLabel}>
+              <span>Includes previous days</span>
+              <Field
+                className={styles.inputTime}
+                name='duration'
+                type='text'
+                placeholder='Duration (days)'
+              />
+            </label>
+
+            <ErrorMessage
+              className={styles.errorTime}
+              component='span'
+              name='duration'
+            />
+          </div>
+          <Button type='submit'>Done</Button>;
+        </Form>
+      </Formik>
+>>>>>>> Stashed changes
     </div>
   );
 };
--- src/components/projectForm/ProjectForm.jsx
+++ src/components/projectForm/ProjectForm.jsx
@@ -1,9 +1,19 @@
+<<<<<<< Updated upstream
 import React from 'react';
+=======
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+>>>>>>> Stashed changes
 import styles from './ProjectForm.module.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { createProject } from '../../redux/projects/projects-operations';
+<<<<<<< Updated upstream
 import projects from '../../redux/projects/projects-reducers';
+=======
+
+import Button from '../../shared/button/Button';
+>>>>>>> Stashed changes
 
 const formSchema = Yup.object().shape({
   name: Yup.string().required('* Project Name is a required field'),
@@ -30,6 +40,7 @@ const ProjectForm = () => {
         <Form className={styles.form}>
           <Field
             className={styles.inputName}
+<<<<<<< Updated upstream
             name="name"
             type="text"
             placeholder="Project name"
@@ -38,22 +49,36 @@ const ProjectForm = () => {
             className={styles.errorName}
             component="span"
             name="name"
+=======
+            name='title'
+            type='text'
+            placeholder='Project name'
+          />
+          <ErrorMessage
+            className={styles.errorName}
+            component='span'
+            name='title'
+>>>>>>> Stashed changes
           />
 
           <Field
             className={styles.inputDescription}
-            name="description"
-            type="text"
-            placeholder="Project description"
+            name='description'
+            type='text'
+            placeholder='Project description'
           />
 
           <ErrorMessage
             className={styles.errorDescription}
-            component="span"
-            name="description"
+            component='span'
+            name='description'
           />
+<<<<<<< Updated upstream
 
           <button type="submit">Submit</button>
+=======
+          <Button type='submit'>Done</Button>
+>>>>>>> Stashed changes
         </Form>
       </Formik>
     </div>
