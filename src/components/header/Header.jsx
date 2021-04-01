9442feb6ad547c939d728623f831a730b2e3d477
Daria Viter <viter.daria@gmail.com>
2021-04-01 10:55:46 +0100

    added link to projects in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Header.module.css';
 import logo from '../../images/Logo-mobile.png';
+import { Link } from 'react-router-dom';
 // import logo from '../../images/Logo-tablet.png';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <a href="">
+      <Link to="/projects">
         <img src={logo} className={styles.headerLogo} alt="Logo" />
-      </a>
+      </Link>
     </div>
   );
 };
