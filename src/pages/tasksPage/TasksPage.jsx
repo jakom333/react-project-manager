5dafdc1cf9327bfd5538bce92efc59f28363da44
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-04-02 09:45:35 +0300

    clear comments

--- src/pages/tasksPage/TasksPage.jsx
+++ src/pages/tasksPage/TasksPage.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-// import { useLocation, useParams } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import moment from 'moment';
+import React from 'react';
 import TaskList from '../../components/taskPageContent/taskList/TaskList';
 import TasktHeader from '../../components/taskPageContent/taskHeader/TaskHeader';
 import MainPageContainer from '../../components/taskPageContainer/mainPageContainer/MainPageContainer';
@@ -11,33 +8,6 @@ import RightItemBar from '../../components/taskPageContainer/rightItemBar/RightI
 import SprintListSideBar from '../../components/taskPageContent/sprintListSideBar/SprintListSideBar';
 
 const TasksPage = () => {
-  // const [filter, setFilter] = useState('');
-  // const { projectId } = useLocation();
-  // const { sprintId } = useParams();
-
-  // const project = useSelector(state =>
-  //   state.projects.find(project => project._id === projectId),
-  // );
-  // const sprint = useSelector(state =>
-  //   state.sprints.find(sprint => sprint._id === sprintId),
-  // );
-
-  // const handleInput = e => {
-  //   setFilter(e.target.value);
-  // };
-
-  // function getDates(startDate, endDate) {
-  //   const dateArray = [];
-  //   let currentDate = moment(startDate);
-  //   const stopDate = moment(endDate);
-  //   while (currentDate <= stopDate) {
-  //     dateArray.push(moment(currentDate).format('MMM Do'));
-  //     currentDate = moment(currentDate).add(1, 'days');
-  //   }
-  //   return dateArray;
-  // }
-  // const dateArr = getDates(sprint.startDate, sprint.endDate);
-
   return (
     <MainPageContainer>
       <LeftBar>
