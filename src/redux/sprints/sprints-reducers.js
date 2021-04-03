46ecf05793b04fd822e1c04f300c1ac86327c882
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-04-04 00:56:02 +0300

    Update sprints-reducers.js

--- src/redux/sprints/sprints-reducers.js
+++ src/redux/sprints/sprints-reducers.js
@@ -9,10 +9,20 @@ import {
   // deleteSprintError,
   // fetchSprintsRequest,
   fetchSprintsSucces,
+  changeTitleSuccess,
   // fetchSprintsError,
 } from './sprints-actions';
 
 const sprints = createReducer([], {
+  [changeTitleSuccess]: (state, { payload }) => {
+    const idx = state.findIndex(item => item._id === payload.sprintId);
+    const sprint = state[idx];
+    return [
+      ...state.slice(0, idx),
+      { ...sprint, title: payload.title },
+      ...state.slice(idx + 1),
+    ];
+  },
   [fetchSprintsSucces]: (_, { payload }) => payload,
   [addSprintSucces]: (state, { payload }) => [...state, payload],
   [deleteSprintSucces]: (state, { payload }) =>
