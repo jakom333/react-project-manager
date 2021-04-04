aff3697ec998ea71937b0a345236cc452f1670fd
Liudmila Popescu <liudmila.popescu91@gmail.com>
2021-04-04 19:35:35 +0100

    date for sprint header

--- src/components/taskPageContent/taskHeader/TaskHeader.jsx
+++ src/components/taskPageContent/taskHeader/TaskHeader.jsx
@@ -6,12 +6,37 @@ import ChangeTitle from '../../titleEditor/TitleEditor';
 // import style from '../sprintList/SprintList.module.css';
 import RoundButton from '../../../shared/roundButton/RoundButton';
 import TaskFilter from '../taskFilter/TaskFilter';
-// import moment from 'moment';
+import { useSelector } from 'react-redux';
+import { getSprintsSelector } from '../../../redux/sprints/sprints-selectors';
+import { useParams } from 'react-router';
 
 export default function SprintHeader() {
   const [showModal, setShowModal] = useState(false);
+  const today = new Date().toJSON().slice(0, 10).split('-').reverse().join('.'); //today
+  const sprints = useSelector(getSprintsSelector);
+  console.log(sprints);
+  const { sprintId } = useParams();
+  console.log(sprintId);
+  const startDate = sprints.find(item => item._id === sprintId).startDate;
+  // console.log(startDate);
 
-  const date = new Date().toJSON().slice(0, 10).split('-').reverse().join('.');
+  const todayReverse = today.split('.').reverse().join('-');
+  // console.log(todayReverse);
+
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // a and b are javascript Date objects
+  function dateDiffInDays(a, b) {
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  }
+  // test it
+  const a = new Date(todayReverse), //today
+    b = new Date(startDate), // startDate
+    difference = dateDiffInDays(a, b);
+  const positiveDifference = Math.abs(difference);
+
+  console.log(difference);
 
   return (
     <div className={styles.headerWrapper}>
@@ -20,12 +45,12 @@ export default function SprintHeader() {
           <div className={styles.controlPanel}>
             <div className={styles.switch}>
               <div className={styles.leftArrow}>&#5176;</div>
-              <span className={styles.day}>1</span>
+              <span className={styles.day}>{positiveDifference}</span>
               <span className={styles.separator}>/</span>
-              <span className={styles.totalDays}>28</span>
+              <span className={styles.totalDays}>4</span>
               <div className={styles.rightArrow}>&#5171;</div>
             </div>
-            <span className={styles.date}>{date}</span>
+            <span className={styles.date}>{today}</span>
           </div>
         </div>
         <div className={styles.control}>
