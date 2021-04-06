dd2b0f7cf98fbd90fb93185cc59029afdbb2fc7e
Bogdan Lytvyn <72852244+BogdanLytvyn@users.noreply.github.com>
2021-04-06 11:41:37 +0300

    fix loader overlay

--- src/redux/projects/projects-reducers.js
+++ src/redux/projects/projects-reducers.js
@@ -13,8 +13,6 @@ import {
   editProjectTitleRequest,
   editProjectTitleSuccess,
   editProjectTitleError,
-  addMemberError,
-  addMemberRequest,
   addMemberSuccess,
 } from './projects-actions';
 
